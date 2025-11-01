9ae164e222e44d96043d80840bce3218022d6fe9
ValeriaYudashkina <valeriayudashkina@gmail.com>
2025-11-01 20:13:47 +0300

    успешное тестирование UDP с локального порта

--- daytime/main.cpp
+++ daytime/main.cpp
@@ -8,35 +8,25 @@ using namespace std;
 int main() {
     sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(13);
-    server_addr.sin_addr.s_addr = inet_addr("129.6.15.28");
+    server_addr.sin_port = htons(8888);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     int s = socket(AF_INET, SOCK_DGRAM, 0);
     if (s == -1) {
         cerr << "Ошибка создания сокета" << endl;
         return 1;
     }
+    cout << "UDP сокет успешно создан" << endl;
 
-    int rc = sendto(s, nullptr, 0, 0, (const sockaddr*)&server_addr, sizeof(sockaddr_in));
+    char msg[] = "Test message\n";
+    int rc = sendto(s, msg, sizeof(msg), 0, (const sockaddr*)&server_addr, sizeof(sockaddr_in));
 
     if (rc == -1) {
         cerr << "Ошибка отправки запроса" << endl;
         close(s);
         return 1;
     }
-
-    char buf[256];
-    sockaddr_in from_addr;
-    socklen_t len = sizeof(sockaddr_in);
-
-    rc = recvfrom(s, buf, sizeof(buf), 0, (sockaddr*)&from_addr, &len);
-
-    if (rc > 0) {
-        buf[rc] = '\0';
-        cout << buf;
-    } else {
-        cerr << "Ошибка получения ответа" << endl;
-    }
+    cout << "Сообщение успешно отправлено" << endl;
 
     close(s);
     return 0;
